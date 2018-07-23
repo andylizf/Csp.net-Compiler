fd636e8361e1d23d5485b7883ec4ced958a606a4
Anti-Li <andylizf@outlook.com>
2018-07-23 20:30:43 +0800

    2.1.2: delete unrelated files. some minor refining.

---
+++ GrammarHelper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Translation.Expression;
+
+namespace GrammarHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            var lineNum = 1;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string csline;
+                try
+                {
+                    csline = Statement.Find(line.Trim()).StatementToCS();
+                }
+                catch (Exception)
+                {
+                    Error.WriteLine($"ERROR: Can't translate! Uncaught SyntaxError: Unexpected token `{line}`.", Error.FontRed);
+                    Error.WriteLine($"意外的标记`{line}`导致的语法错误。", Error.FontWhite);
+                    continue;
+                }
+
+                Error.WriteLine($"Line {lineNum}: ", Error.FontBlue);
+                Error.WriteLine(csline, Error.FontWhite);
+                lineNum++;
+            }
+        }
+    }
+}
